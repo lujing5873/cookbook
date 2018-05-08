c02c331a559551c76acc52fed7bd238556e69da3
nchz500 <743566091@qq.com>
2018-05-08 11:51:53 +0800

    更新主页面

--- app/src/main/java/com/zxx/cookbook/activity/MainActivity.java
+++ app/src/main/java/com/zxx/cookbook/activity/MainActivity.java
@@ -1,15 +1,129 @@
 package com.zxx.cookbook.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.zxx.cookbook.R;
+import com.zxx.cookbook.adapter.ViewPagerAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tab_1)
+    RadioButton tab1;
+    @BindView(R.id.tab_2)
+    RadioButton tab2;
+    @BindView(R.id.tab_3)
+    RadioButton tab3;
+    @BindView(R.id.tab_4)
+    RadioButton tab4;
+    @BindView(R.id.main_vp)
+    ViewPager mainVp;
+    private List<Fragment> mList;
+    private ViewPagerAdapter mViewPagerAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
+
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState) {
+        Drawable drawableFirst = getResources().getDrawable(R.drawable.bottom1_img);
+        drawableFirst.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        tab1.setCompoundDrawables(null, drawableFirst, null, null);//只放上面
+
+        Drawable drawableFirst2 = getResources().getDrawable(R.drawable.bottom2_img);
+        drawableFirst2.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        tab2.setCompoundDrawables(null, drawableFirst2, null, null);//只放上面
+
+
+        Drawable drawableFirst3 = getResources().getDrawable(R.drawable.bottom3_img);
+        drawableFirst3.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        tab3.setCompoundDrawables(null, drawableFirst3, null, null);//只放上面
+
+
+        Drawable drawableFirst4 = getResources().getDrawable(R.drawable.bottom4_img);
+        drawableFirst4.setBounds(0, 0, 69, 69);//第一0是距左右边距离，第二0是距上下边距离，第三69长度,第四宽度
+        tab4.setCompoundDrawables(null, drawableFirst4, null, null);//只放上面
+
+        mList = new ArrayList<>();
+        Fragment fragment=new Fragment();
+        Fragment fragment2=new Fragment();
+        Fragment fragment3=new Fragment();
+        Fragment fragment4=new Fragment();
+        mList.add(fragment);
+        mList.add(fragment2);
+        mList.add(fragment3);
+        mList.add(fragment4);
+        mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), mList);
+        mainVp.setAdapter(mViewPagerAdapter);
+        mainVp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                onPageChange(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    public void onPageChange(int position) {
+        switch (position) {
+            case 0:
+                tab1.setChecked(true);
+                break;
+            case 1:
+                tab2.setChecked(true);
+                break;
+            case 2:
+                tab3.setChecked(true);
+                break;
+            case 3:
+                tab4.setChecked(true);
+                break;
+        }
+    }
+
+
+    @OnClick({R.id.tab_1, R.id.tab_2, R.id.tab_3, R.id.tab_4})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tab_1:
+                mainVp.setCurrentItem(0);
+                break;
+            case R.id.tab_2:
+                mainVp.setCurrentItem(1);
+                break;
+            case R.id.tab_3:
+                mainVp.setCurrentItem(2);
+                break;
+            case R.id.tab_4:
+                mainVp.setCurrentItem(3);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/zxx/cookbook/adapter/ViewPagerAdapter.java
@@ -0,0 +1,29 @@
+package com.zxx.cookbook.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * 主页面
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mList;
+    public ViewPagerAdapter(FragmentManager fm,List<Fragment> mList) {
+        super(fm);
+        this.mList=mList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+}
