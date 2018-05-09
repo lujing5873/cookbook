76c6758ae962c6b929f60d4cf3582b142de71d0f
wangwenzhang <wwz19900222@163.com>
2018-05-09 10:39:38 +0800

    第二次

--- app/src/main/java/com/zxx/cookbook/fragment/SearchFragment.java
+++ app/src/main/java/com/zxx/cookbook/fragment/SearchFragment.java
@@ -1,18 +1,43 @@
 package com.zxx.cookbook.fragment;
 
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zxx.cookbook.R;
+import com.zxx.cookbook.adapter.SearchAdpater;
+import com.zxx.cookbook.bean.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by MDM on 2018/5/8.
  */
 
 public class SearchFragment extends BaseFragment {
+    @BindView(R.id.search_recycler)
+    RecyclerView searchRecycler;
+    private SearchAdpater adpater;
+    private List<Food> foods = new ArrayList<>();
+
     @Override
     public int getLayoutId() {
-        return 0;
+        return R.layout.fragment_search;
     }
 
     @Override
     public void initView() {
-
+        searchRecycler.setLayoutManager(new LinearLayoutManager(activity));
+        adpater=new SearchAdpater(activity,foods);
+        searchRecycler.setAdapter(adpater);
     }
 
     @Override
@@ -22,6 +47,13 @@ public class SearchFragment extends BaseFragment {
 
     @Override
     public void initLisenter() {
+        adpater.setOnItemClickLinsentener(new SearchAdpater.OnItemClickLisentener() {
+            @Override
+            public void onItemClick(int position) {
 
+            }
+        });
     }
+
+
 }
